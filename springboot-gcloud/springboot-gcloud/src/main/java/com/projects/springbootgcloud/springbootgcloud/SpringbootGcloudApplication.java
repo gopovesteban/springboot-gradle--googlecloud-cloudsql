6d1ea3898e5b20ae7cc518d02b8578c82d2c85ac
Esteban <gopovesteban2@gmail.com>
2018-12-04 11:34:41 -0600

    Initializer

---
+++ springboot-gcloud/springboot-gcloud/src/main/java/com/projects/springbootgcloud/springbootgcloud/SpringbootGcloudApplication.java
@@ -0,0 +1,12 @@
+package com.projects.springbootgcloud.springbootgcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGcloudApplication.class, args);
+	}
+}
